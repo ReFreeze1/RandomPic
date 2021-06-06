ddfac56e3ebf3434a790d4ddb8d8f664e08f96f5
ReFreeze1 <81642777+ReFreeze1@users.noreply.github.com>
2021-06-06 12:29:55 +0200

    ready

--- RandomPic.cpp
+++ RandomPic.cpp
@@ -4,8 +4,21 @@
 
 int main()
 {
+    int width = 0;
+    int height = 0;
     std::srand(std::time(nullptr));
     std::ofstream file("pic.txt");
+    std::cout << "width: ";
+    std::cin >> width;
+    std::cout << "height:";
+    std::cin >> height;
 
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int temp = rand() % 100;
+            file << ((temp % 2) == 0) ? 0 : 1;
+        }
+        file << std::endl;
+    }
     file.close();
 }
